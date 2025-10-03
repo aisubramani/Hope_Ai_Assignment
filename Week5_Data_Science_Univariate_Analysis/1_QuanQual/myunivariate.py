dd079fae816d5d7a8a3991961940590bd762ebb7
Dr Subramani Suresh <suresh1info@gmail.com>
2025-10-03 12:29:30 +0530

    yes

--- Week5_Data_Science_Univariate_Analysis/1_QuanQual/myunivariate.py
+++
@@ -1,8 +0,0 @@
-#univariate Class
-class myunivariate():
-    
-    #QuanQual list split
-    def QuanQual(dataset):
-        quan=[x for x in dataset if dataset[x].dtype!=object]
-        qual=[x for x in dataset if dataset[x].dtype==object]
-        return quan,qual
